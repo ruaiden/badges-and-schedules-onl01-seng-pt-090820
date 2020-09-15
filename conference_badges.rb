e0304d6cd82f2b00d87d4e104cc153f7c0e40c49
Aiden Marques <aiden.marques94@gmail.com>
2020-09-15 19:02:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+#def badge_maker(name)
+#  "Hello, my name is #{name}."
+#end
+
+def batch_badge_creator(attendees)
+  attendees=[]
+  attendees.each do |badges|
+     printf "Hello, my name is #{badges}."
+  end
+ 
+end
+
