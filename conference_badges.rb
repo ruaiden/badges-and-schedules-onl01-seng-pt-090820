dc6c1466fdb9889868ef314971ebe683564d940d
Aiden Marques <aiden.marques94@gmail.com>
2020-09-15 20:51:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,5 +7,5 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(room)
-  roome.each_with_index {|number| "Hello #{name}! You'll be assigned to room #{number}!"}
+  room.each_with_index {|number| "Hello #{name}! You'll be assigned to room #{number}!"}
 end
