64a56f930c7c08ab11adaf2461c172e854a86bcc
Aiden Marques <aiden.marques94@gmail.com>
2020-09-15 20:40:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
+def batch_badge_creator(names)
   names.map{|name| badge_marker(name)}
   #new_attendees=[]
   #attendees.each do |badges| 
